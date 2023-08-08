8c9ebf95538ad08013eb39fe6ecd2d37d916ed5a
d3uceY <onyekwelujesse1234@gmail.com>
2023-08-08 12:33:19 -0700

    added more animations

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ menuCloseButton.addEventListener('click', () => {
 })
 
 //gsap animations
-
+gsap.registerPlugin(ScrollTrigger)
 
 
 gsap.from('.navigation-logo', {
   opacity: 0,
-  duration: .5,
-  y: '20%',
+  duration: 1,
+  y: '30%',
 })
 
 gsap.from('.navigation-item', {
     opacity: 0,
-    duration: .5,
+    duration: .6,
     y: '20%',
     stagger: .1,
     delay: .5
@@ -34,6 +34,8 @@ gsap.from('.navigation-item', {
     opacity: 0,
     y: '20%',
     stagger: .1,
+    duration: .6,
+    delay: 1 
   })
 
   gsap.from('.hero-content > h1', {
@@ -46,18 +48,117 @@ gsap.from('.navigation-item', {
     opacity: 0,
     y: '50%',
     duration: .5,
-    delay: .9
+    delay: .9,
+    ease: "power3.out"
   })
 
   gsap.from('.hero-content > button', {
     opacity: 0,
     y: '50%',
     duration: .5,
-    delay: .9
+    delay: 1.2,
+    ease: "power3.out"
   })
 
   gsap.from('.hero-image-container', {
     opacity: 0,
    width: '30%',
-   duration: .5,
+   duration: 1.2  ,
+   ease: "power3.out"
   })
+
+  gsap.from('.collection-header', {
+     opacity: 0,
+     y:'20%',
+     duration: 1.5,
+     ease: "power3.out",
+     scrollTrigger: '.collection-header'
+  })
+
+  gsap.from('.collection-paragraph', {
+    opacity: 0,
+    y:'20%',
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: '.collection-paragraph'
+ })
+
+ gsap.from('.collections-container', {
+  opacity: 0.1,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.collections-container'
+})
+
+gsap.from('.arrival-header', {
+  opacity: 0,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.arrival-header'
+})
+
+
+gsap.from('.arrival-paragraph', {
+  opacity: 0,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.arrival-paragraph'
+})
+
+
+gsap.from('.hoodie-block', {
+  opacity: 0.01,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  stagger:.2,
+  scrollTrigger: '.hoodie-block'
+})
+
+gsap.from('.categories-header', {
+  opacity: 0,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.categories-header'
+})
+
+gsap.from('.categories-paragraph', {
+  opacity: 0,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.categories-paragraph'
+})
+
+gsap.from('.categories-container', {
+  opacity: 0,
+  y:'20%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.categories-container'
+})
+
+gsap.from('.coming-soon-write-up', {
+  opacity: 0,
+  y:'30%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.coming-soon-write-up'
+})
+
+gsap.from('.comming-soon-image', {
+  opacity: 0,
+  x:'30%',
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: '.comming-soon-image'
+})
+
+
+
+
+
