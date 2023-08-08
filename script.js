5dc08e1740ac6920a652325838acf86d3a2fb17b
d3uceY <onyekwelujesse1234@gmail.com>
2023-08-08 10:14:40 -0700

    i have made chnages to this repository

--- script.js
+++ script.js
@@ -12,6 +12,52 @@ menuCloseButton.addEventListener('click', () => {
     navBar.classList.remove('clicked');
 })
 
-// gsap animations
-gsap.registerPlugin(ScrollTrigger);
+//gsap animations
 
+
+
+gsap.from('.navigation-logo', {
+  opacity: 0,
+  duration: .5,
+  y: '20%',
+})
+
+gsap.from('.navigation-item', {
+    opacity: 0,
+    duration: .5,
+    y: '20%',
+    stagger: .1,
+    delay: .5
+  })
+
+  gsap.from('.navigation-icons', {
+    opacity: 0,
+    y: '20%',
+    stagger: .1,
+  })
+
+  gsap.from('.hero-content > h1', {
+    opacity: 0,
+    x: '20%',
+    delay: .8
+  })
+
+  gsap.from('.hero-content > p', {
+    opacity: 0,
+    y: '50%',
+    duration: .5,
+    delay: .9
+  })
+
+  gsap.from('.hero-content > button', {
+    opacity: 0,
+    y: '50%',
+    duration: .5,
+    delay: .9
+  })
+
+  gsap.from('.hero-image-container', {
+    opacity: 0,
+   width: '30%',
+   duration: .5,
+  })
